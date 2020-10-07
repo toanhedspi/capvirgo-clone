7168d96d3e19fd913b8ef6bcd622d6beefd98a95
Toan Nguyen <toannd@miichisoft.com>
2020-10-07 11:59:03 +0700

    Init Project File Structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
